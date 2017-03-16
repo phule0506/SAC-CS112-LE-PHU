1ec0ee177341df470e670be97c622f2a8cad4c1a
phule0506 <giaphu0506@gmail.com>
2017-03-15 17:29:02 -0700

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Random;
+package com.whatstek;
+public class Game
+{
+    public static void main ( String[] args) 
+{
+        Random random = 
+        Scanner input = new Scanner(System.in);
+        int random = rand(1,3) ; //assign 1 = hammer, 2 = scissor, 3 = paper 
+        int game ;
+         game = input.nextInt();
+         if (game == 1);
+         {
+            while ( random == 2 );
+            {System.out.println(" Lose"); }
+         }
+       
+
+                  
+         }
+}
+}
+                 
+             
