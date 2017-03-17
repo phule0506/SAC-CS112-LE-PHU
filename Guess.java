10b8aa0aea7475d5d816c955fed692e56fdf2093
phule0506 <giaphu0506@gmail.com>
2017-03-16 22:40:06 -0700

    Update Guess.java

--- Guess.java
+++ Guess.java
@@ -1,18 +1,61 @@
 import java.util.Scanner;
-public class Guess
+public class Game3
 {
-  public static void main(String args[])
-{
-  Scanner input= new Scanner(System.in);
-  int number;
-  int numberrd;
-  numberrd = 0 + (int)(Math.random() * 9);
-  System.out.print(" Chose a number from 0-9 : ");
-  number = input.nextInt();
-  if ( number == numberrd )
-  { System.out.println(" You Won ");
-  } else {
-    System.out.println(" You Lose");
-  }
-}
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+            int ng1;
+            int ng2;
+            System.out.print(" Player A please pick 1 = hammer, 2 = scissor, 3 = paper :");
+                                 ng1 = input.nextInt() ;
+            System.out.print(" Player B please pick 1 = hammer, 2 = scissor, 3 = paper :");
+            ng2 = input.nextInt() ;
+        if (ng1 == 1 )
+        {
+            if ( ng2 == 1) 
+            {
+                System.out.println(" Draw " );
+            }
+            else if ( ng2 == 2 )
+            {
+                System.out.println(" You Won " );
+            }
+            else 
+            {
+                System.out.println("You Lose");
+            }
+        }
+         else if ( ng1 == 2 )
+        {
+             if ( ng2 == 1) 
+            {
+                System.out.println(" You Lose " );
+            }
+            else if ( ng2 == 2 )
+            {
+                System.out.println(" Draw " );
+            }
+            else 
+            {
+                System.out.println("You Won");
+            }
+        }
+         else if ( ng1 == 3 )
+        {
+             if ( ng2 == 1) 
+            {
+                System.out.println(" You Won " );
+            }
+            else if ( ng2 == 2 )
+            {
+                System.out.println(" You Lose " );
+            }
+            else 
+            {
+                System.out.println("Draw");
+            }
+        }
+         
+         input.close();
+    }
 }
