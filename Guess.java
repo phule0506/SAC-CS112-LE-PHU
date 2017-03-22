388027c7f3533c76d2e58bce4ebafb512ed7c7c3
phule0506 <giaphu0506@gmail.com>
2017-03-22 16:28:26 -0700

    Add files via upload

---
+++ Guess.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Guess
+{
+  public static void main(String args[])
+{
+  Scanner input= new Scanner(System.in);
+  int number;
+  int numberrd;
+  numberrd = 0 + (int)(Math.random() * 9);
+  System.out.print(" Chose a number from 0-9 : ");
+  number = input.nextInt();
+  if ( number == numberrd )
+  { System.out.println(" You Won ");
+  } else {
+    System.out.println(" You Lose");
+  }
+  input.close();
+}
+}
