6d4054aeed97ae8e5e041f2592f83853440428f4
phule0506 <giaphu0506@gmail.com>
2017-03-22 20:06:47 -0700

    Update Guess.java

--- Guess.java
+++ Guess.java
@@ -3,17 +3,28 @@ public class Guess
 {
   public static void main(String args[])
 {
-  Scanner input= new Scanner(System.in);
+
+  int count = 1 ;
   int number;
   int numberrd;
+  
+  String a = " You Won";
+  String b = " You Lose";
+  //for ( int count = 1 ; count < 4 ; count++)
+ 
+  do {
+   System.out.printf("Round %d\n", count);
+  Scanner input= new Scanner(System.in);
+
   numberrd = 0 + (int)(Math.random() * 9);
   System.out.print(" Chose a number from 0-9 : ");
   number = input.nextInt();
-  if ( number == numberrd )
-  { System.out.println(" You Won ");
-  } else {
-    System.out.println(" You Lose");
-  }
+  
+  String ss = ( number == numberrd) ? a : b;
+  System.out.println(ss);
+  count++ ; 
   input.close();
 }
+  while ( count < 4 );
+  }
 }
