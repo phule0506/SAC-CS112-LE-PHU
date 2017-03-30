cf6ee4c668135d34a91c42179c4318dd8cb588d6
phule0506 <giaphu0506@gmail.com>
2017-03-29 21:39:56 -0700

    Add files via upload

--- Oddeven.java
+++ Oddeven.java
@@ -1,46 +1,50 @@
 //import java.util.Scanner;
 import javax.swing.JOptionPane;
+import java.util.Arrays;
 public class Oddeven
 {
     public static void main(String[] args)
     {
-       int[] evennumber = new int [4];
-       int[] oddnumber = new int [4] ;
-       int i;
-       oddnumber[0]=1;
-       oddnumber[1]=3;
-       oddnumber[2]=5;
+       int[] evennumber = new int[4];
        evennumber[0] = 0;
        evennumber[1] = 2;
        evennumber[2] = 4;
        evennumber[3] = 6;
+       int[] oddnumber = new int[4] ;
+       oddnumber[0]=1;
+       oddnumber[1]=3;
+       oddnumber[2]=5;
+       int i;
+      
+       
        String a = " Win ", b = " Lose " ;
        int numberrd ;
-       //Scanner input = new Scanner(System.in);
+ 
         
-       do
-       {
+       //do
+       //{
            numberrd = 0 +(int)(Math.random() * 6 );
-           for(i=0;i<
-           String ssodd = (numberrd == oddnumber[]) ? a : b;
-           String sseven =(numberrd == evennumber[]) ? a : b;
-           String oddeven =JOptionPane.showInputDialog("Rolling the dice...\nNumber betwwn 0 to 6\nPlease pick ODD or EVEN:");
+           String oddeven = JOptionPane.showInputDialog("Rolling the dice...\nNumber betwwn 0 to 6\nPlease pick ODD or EVEN:");
+           
+           // ODD
+           for(i=0;i < oddnumber.length;i++)
+           {
+             String ssodd = (numberrd == oddnumber[i]) ? a : b;
+         //  String sseven =(numberrd == evennumber[]) ? a : b;
+           
                 if (oddeven.toUpperCase().equals("ODD"));
                 {
                     System.out.println(ssodd);
                
                     
                 }
+           }
+
             
-            
-            
-            
-       }
-       while(true);
+      // }
+      // while(true);
         
-        
-        input.close();
-        
+     
     }
     
     
