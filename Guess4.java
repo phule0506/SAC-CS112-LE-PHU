4fb10c6093a57663e934d35d204a169dbd15dd38
phule0506 <giaphu0506@gmail.com>
2017-03-30 23:26:30 -0700

    Add files via upload

--- Guess4.java
+++ Guess4.java
@@ -5,7 +5,8 @@ public class Guess4
 {
 public static void main(String args[])
 {
- int guesspercent,goodguess = 0,badguess = 0,guessnumber = 0,number,numberrd, count = 1  ;
+ int guesspercent,goodguess = 0,badguess = 0,guessnumber = 0,number, count = 1  ;
+ int inputnumber;
 String a = " You Won", b = " You Lose";
 RandomNumber mynumberrd = new RandomNumber();// create a RandomNumber object and assign it to mynumberrd
 Scanner input= new Scanner(System.in);
@@ -13,11 +14,13 @@ Scanner input= new Scanner(System.in);
 do {
 // if numberrd is not put under the DO statement, it will keeps the same numberrd
 //numberrd = 0 + (int)(Math.random() * 9); 
-int rnum = mynumberrd.Getanumberbetween1and10();//call RandomNumber's Getanumberbetween1and10() method; then assignt it 
-                                                //to int rnum
+    System.out.print("Please choose the maximun number:");
+    inputnumber = input.nextInt();
+int rnum = mynumberrd.GetANumber(inputnumber);//call RandomNumber's Getanumberbetween1and10() method; then assignt it to int rnum
 
+                                               
 System.out.printf("Round %d\n", count);
-System.out.print(" Chose a number from 0-9 : ");
+System.out.printf(" Chose a number from 0-%d : ", inputnumber);
 number = input.nextInt();
 String ss = ( number == rnum) ? a : b;
 System.out.println(ss);
--- RandomNumber.java
+++ RandomNumber.java
@@ -1,10 +1,10 @@
-
 public class RandomNumber// create a new class
 {
-  public int Getanumberbetween1and10()//calling a method
+    public int GetANumber (int high)
+  //public int Getanumberbetween1and10()//calling a method
   {
     int numberrd;
-    numberrd = 0 + (int)(Math.random() * 9); 
+    numberrd = 0 + (int)(Math.random() * high); 
     return numberrd;//need to return the value
   }
 }
