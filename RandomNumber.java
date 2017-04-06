92962722718eaf7d9a1b6cd52aad30c35ef084b4
phule0506 <giaphu0506@gmail.com>
2017-04-05 20:03:26 -0700

    Update RandomNumber.java

--- RandomNumber.java
+++ RandomNumber.java
@@ -1,3 +1,4 @@
+
 public class RandomNumber// create a new class
 {
     public int GetANumber (int high)
@@ -5,6 +6,20 @@ public class RandomNumber// create a new class
   {
     int numberrd;
     numberrd = 0 + (int)(Math.random() * high); 
+    return numberrd;//need to return the value
+  }
+        public int GetANumber (int lo, int hi)
+  //public int Getanumberbetween1and10()//calling a method
+  {
+    int numberrd;
+    numberrd = lo + (int)(Math.random() * (hi-lo)); 
+    return numberrd;//need to return the value
+  }
+           
+  public int Getanumberbetween1and10()//calling a method
+  {
+    int numberrd;
+    numberrd = 0 + (int)(Math.random() * 10); 
     return numberrd;//need to return the value
   }
 }
