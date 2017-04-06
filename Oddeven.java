8c765b2269674ba7f216c68cc65b6c437e7510d9
phule0506 <giaphu0506@gmail.com>
2017-04-05 20:02:49 -0700

    Update Oddeven.java

--- Oddeven.java
+++ Oddeven.java
@@ -3,49 +3,50 @@ import javax.swing.JOptionPane;
 import java.util.Arrays;
 public class Oddeven
 {
-    public static void main(String[] args)
+  public static void main(String[] args)
+  {
+    int[] evennumber = new int[4];
+    evennumber[0] = 0;
+    evennumber[1] = 2;
+    evennumber[2] = 4;
+    evennumber[3] = 6;
+    int[] oddnumber = new int[4] ;
+    oddnumber[0]=1;
+    oddnumber[1]=3;
+    oddnumber[2]=5;
+    int i;
+    
+    
+    String a = " Win ", b = " Lose " ;
+    int numberrd ;
+    
+    
+    //do
+    //{
+    numberrd = 0 +(int)(Math.random() * 6 );
+    String oddeven = JOptionPane.showInputDialog("Rolling the dice...\nNumber betwwn 0 to 6\nPlease pick ODD or EVEN:");
+    
+    // ODD
+    for(i=0;i < oddnumber.length;i++)
     {
-       int[] evennumber = new int[4];
-       evennumber[0] = 0;
-       evennumber[1] = 2;
-       evennumber[2] = 4;
-       evennumber[3] = 6;
-       int[] oddnumber = new int[4] ;
-       oddnumber[0]=1;
-       oddnumber[1]=3;
-       oddnumber[2]=5;
-       int i;
+      String ssodd = (numberrd == oddnumber[i]) ? a : b;
+      String sseven =(numberrd == evennumber[i]) ? a : b;
       
-       
-       String a = " Win ", b = " Lose " ;
-       int numberrd ;
- 
+      if (oddeven.toUpperCase().equals("ODD"))
+      {
+        System.out.println(ssodd);            
         
-       //do
-       //{
-           numberrd = 0 +(int)(Math.random() * 6 );
-           String oddeven = JOptionPane.showInputDialog("Rolling the dice...\nNumber betwwn 0 to 6\nPlease pick ODD or EVEN:");
-           
-           // ODD
-           for(i=0;i < oddnumber.length;i++)
-           {
-             String ssodd = (numberrd == oddnumber[i]) ? a : b;
-         //  String sseven =(numberrd == evennumber[]) ? a : b;
-           
-                if (oddeven.toUpperCase().equals("ODD"));
-                {
-                    System.out.println(ssodd);
-               
-                    
-                }
-           }
-
-            
-      // }
-      // while(true);
-        
-     
+      }else 
+        {System.out.println(sseven);
+        }
+      }
     }
     
     
-}
+    // }
+    // while(true);
+    
+    
+  }
+  
+  
