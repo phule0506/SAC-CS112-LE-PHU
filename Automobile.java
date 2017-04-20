0c1f7e0632d04b17f403378b6fa5d527f082c1c3
phule0506 <giaphu0506@gmail.com>
2017-04-19 21:36:04 -0700

    Add files via upload

---
+++ Automobile.java
@@ -0,0 +1,25 @@
+
+public class Automobile {
+	
+	public String SetName(String n) {
+		return n;
+	}
+
+	public String SetColor(String c) {
+		return c;
+	}
+	public int SetNumberOfDoors(int d){
+		return d;
+	}
+	public int SetNumberOfCylinders(int cy){
+		return cy;
+	}
+	public int Transmission(int t){
+		return t;
+	}
+	public String About(){
+		
+	}
+	
+
+}
